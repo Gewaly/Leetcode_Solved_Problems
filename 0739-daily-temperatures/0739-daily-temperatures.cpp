562815c990c835554f3d7ae39dea57fc2814c960
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2022-12-19 00:45:56 +0200

    Time: 489 ms (17.09%), Space: 102.8 MB (53.94%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+ 
+        stack<int>s;
+        vector<int>arr(temperatures.size(),0);
+        for (int i=0;i<temperatures.size();i++) {
+        while (!s.empty()&&temperatures[s.top()]<temperatures[i]) {
+            arr[s.top()]=i-s.top();
+            s.pop();
+        }
+        s.push(i);
+    }
+        return arr;
+        
+        
+        
+    }
+};
