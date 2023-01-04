b9e0acb3556b581d663d48188ac509f5aaf6f103
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-01-04 17:18:57 +0200

    Time: 1005 ms (33.34%), Space: 0B (100.00%) - LeetHub

--- 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 SELECT  date_id  , make_name ,  COUNT(DISTINCT lead_id) AS  unique_leads, 
 COUNT(DISTINCT partner_id ) AS  unique_partners 
 FROM DailySales 
