c1679dfa42746df8f6b52802fa07a00cce3aa7f3
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-01-09 19:49:12 +0200

    Time: 44 ms (73.22%), Space: 22 MB (26.44%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  
+    int largestPerimeter(vector<int>& nums) {
+         int n=nums.size();
+        sort(nums.begin(),nums.end());
+       for (int i =n - 1; i > 1; --i){
+            if (nums[i] < nums[i - 1] + nums[i - 2])
+                return nums[i] + nums[i - 1] + nums[i - 2];
+       }
+                return 0;
+
+    }
+};
