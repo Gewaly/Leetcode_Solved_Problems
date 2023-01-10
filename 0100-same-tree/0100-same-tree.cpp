7a6c39956917f3a398dc2ff3b0aff38743ef71a0
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-01-10 17:20:08 +0200

    Time: 3 ms (65.62%), Space: 10 MB (48.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,7 +14,7 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==nullptr&&q==nullptr) return true;
         if(p!=nullptr&&q!=nullptr)
-        return(p->val==q->val&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right)) ;
+        if(p->val==q->val&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right))  return true;
             
        return false;     
     }
