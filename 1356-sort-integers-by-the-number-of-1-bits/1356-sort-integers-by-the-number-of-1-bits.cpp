355cb574f0e2ab04e4398732069b466392aaab5f
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-01-17 14:06:21 +0200

    Time: 8 ms (86.43%), Space: 10.2 MB (50.60%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,11 +1,9 @@
 class Solution {
- public:
-  vector<int> sortByBits(vector<int>& arr) {
-    sort(begin(arr), end(arr), []( int a, int b) {
-       int x = bitset<32>(a).count();
-       int y = bitset<32>(b).count();
-      return x == y ? a < b : x < y;
-    });
-    return arr;
-  }
+public:
+   	vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(),[](const int& a, const int& b){
+            int countA = __builtin_popcount(a), countB = __builtin_popcount(b);
+            return countA==countB ? a<b:countA<countB;});
+        return arr;
+    }
 };
