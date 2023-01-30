790fd2f87659a236f0a398bae921eb1258baa098
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-01-30 13:50:25 +0200

    Time: 0 ms (100.00%), Space: 6.2 MB (22.73%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        else if(n==1 || n==2) return 1;       
+        vector<int>arr(n+1);
+        arr[0]=0; arr[1]=1; arr[2]=1;
+        for(int i=3;i<n+1;i++){
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        }
+        return arr[n];
+    }
+};
