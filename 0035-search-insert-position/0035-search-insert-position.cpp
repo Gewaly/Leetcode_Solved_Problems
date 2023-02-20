59409979edae1e81e65e0d0425961ad0318acb6e
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-02-20 02:19:57 +0200

    Time: 3 ms (90.12%), Space: 9.7 MB (77.52%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,14 +1,19 @@
+// Author : Mohamed Gewaly
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        
       for(int i=0;i<nums.size();i++){
-          if(nums[i]==target){  return i; }
+          if(nums[i]==target){ //if the target is found
+              return i;
+          }
       }  
+        // if not found && not the last element 
      for(int i=0;i<nums.size();i++){
-         if(nums[i]>target) return i;
+         if(nums[i]>target) return i; 
          
      }
+        // if not found && the last element 
         return nums.size();
-
     }
 };
