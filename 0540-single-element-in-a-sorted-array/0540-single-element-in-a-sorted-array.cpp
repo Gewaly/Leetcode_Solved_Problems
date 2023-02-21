d302ceb07614b58fadcf236a5f167a4e52a27cdf
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-02-21 21:03:58 +0200

    Time: 38 ms (20.02%), Space: 22.3 MB (93.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         map<int,int>mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
+        if(nums.size()==1) return nums[0];
+        for(int i=0;i<nums.size()-1;i+=2){
+          //  mpp[nums[i]]++;
+            if(nums[i]!=nums[i+1]) return nums[i];
         }
-        for(auto x: mpp) {
-            if(x.second==1) return x.first;
-        }
-        return 1;
+        //for(auto x: mpp) {
+          //  if(x.second==1) return x.first;
+        //}
+        return nums[nums.size()-1];
         
     }
 };
