958714ef05c24657852478bc24745f3284c809d2
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-02-21 21:10:13 +0200

    Time: 23 ms (91.47%), Space: 22.4 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,16 +1,18 @@
+//Author: Mohamed Gewaly
 class Solution {
 public:
+    /** 
+    We look at evne indicies and check if the number is equal to the number on its right.
+    if not equal return this number at even index,
+    else, the number is the last element.
+    **/
     int singleNonDuplicate(vector<int>& nums) {
-        map<int,int>mpp;
-        if(nums.size()==1) return nums[0];
-        for(int i=0;i<nums.size()-1;i+=2){
-          //  mpp[nums[i]]++;
-            if(nums[i]!=nums[i+1]) return nums[i];
+        if(nums.size()==1) return nums[0]; // if there one element 
+        for(int i=0;i<nums.size()-1;i+=2){   
+            if(nums[i]!=nums[i+1]) return nums[i]; // if not equal return this number at even index,
         }
-        //for(auto x: mpp) {
-          //  if(x.second==1) return x.first;
-        //}
-        return nums[nums.size()-1];
+
+        return nums[nums.size()-1]; // the number is the last element. 
         
     }
 };
