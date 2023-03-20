cff7e035d4fd779fc57c7850b3e6f09e48cdfb59
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-03-20 15:28:36 +0200

    Time: 25 ms (23.93%), Space: 20.3 MB (76.11%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+       /** int cnt=0,mx=0;
+        for(int i=0;i<flowerbed.size();i++){
+            if(flowerbed[i]==0){
+                cnt++; mx=max(mx,cnt);
+            }
+            else cnt=0;
+        }
+        cout<<mx<<endl;
+        mx-=2;
+        float f=ceil(mx/2.0);
+        cout<<f<<endl;
+        if(f>=n) return true;
+        else return false;
+**/
+        for(int i=0;i<flowerbed.size();i++)
+        {
+            if(flowerbed[i]==0)
+            {
+                if((i==0 || flowerbed[i-1]==0)&&(i==flowerbed.size()-1 || flowerbed[i+1]==0))
+                {
+                    n--;
+                    flowerbed[i]=1;
+                }
+            }
+        }
+        return (n<=0);
+        
+    }
+};
