373e2342c6b6456d0298fbbc4081a77a727bc181
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-04-25 14:59:16 +0200

    Time: 3 ms (56.46%), Space: 5.9 MB (93.16%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long ans=0;
+        while(x!=0){
+            ans=ans*10+x%10;
+            x/=10;
+            if(abs(ans)>INT_MAX) return 0;
+            
+        }
+        return ans;
+        
+    }
+};
