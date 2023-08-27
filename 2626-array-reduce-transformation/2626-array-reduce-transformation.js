5e6269b57089951d8179104a1855fb18c4851df6
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-08-27 16:27:25 +0300

    Time: 46 ms (95.33%), Space: 42.2 MB (73.04%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let res=nums.reduce(fn,init);
+    return res;
+};
