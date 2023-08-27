1e06a83f85f3754905f2a2cbbe4dd3dc1550cb8f
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-08-27 16:12:20 +0300

    Time: 66 ms (7.45%), Space: 41.7 MB (78.07%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let res=[];
+    for(let i=0;i<arr.length;i++){
+        res[i]=fn(arr[i],i);
+    }
+    return res;
+    
+};
