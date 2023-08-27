3aabdba70715c8a7277aed5495d73799d061f42a
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-08-27 16:23:10 +0300

    Time: 41 ms (97.98%), Space: 42.4 MB (5.15%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,7 +4,10 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let res=arr.filter(fn);
+    let res=[];
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)) res.push(arr[i]);
+    }
     return res;
     
 };
