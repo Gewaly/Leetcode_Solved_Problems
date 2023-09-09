b59a7e7e69c0a3785fce80bc811d53669c309024
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-09-10 01:13:29 +0300

    Time: 0 ms (100.00%), Space: 6.6 MB (85.79%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int>dp(target+1);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(auto num :nums){
+                if(i-num>=0) dp[i]+=dp[i-num];
+            }
+            
+        }
+        return dp[target];
+        
+    }
+};
