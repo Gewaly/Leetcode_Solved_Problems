e442cb79ec9836bc095860f09c924d35afb25b5d
Mohamed Atef Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-09-28 23:32:33 +0300

    Time: 14 ms (13.60%), Space: 17.3 MB (6.67%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n);
+        deque<int>deq;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0) deq.push_front(nums[i]);
+            else deq.push_back(nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            ans[i]=deq[i];
+        }
+        return ans;
+        
+    }
+};
