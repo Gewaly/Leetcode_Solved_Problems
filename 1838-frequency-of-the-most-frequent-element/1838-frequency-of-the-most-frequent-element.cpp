e4393dd441a5234872e8348cbc4ae4edf55d25a0
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-11-18 13:57:50 +0200

    Time: 174 ms (44.80%), Space: 99.3 MB (85.20%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -6,8 +6,7 @@ public:
         long window_sum = 0,window_size = 0,result = 0,pos = 0;
         for(int i = 0;i<nums.size();i++)
         {
-            window_sum+=nums[i];
-            window_size = i-pos+1;
+            window_sum+=nums[i];  window_size = i-pos+1;
             while(( nums[i]*window_size - window_sum ) > k)  window_sum-=nums[pos++], window_size--;
             
             result = max(result,window_size);
