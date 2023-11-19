fceca3d7b4a0436c2944224a6151cccb46e1eb58
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-11-19 17:58:57 +0200

    Time: 187 ms (82.61%), Space: 53.3 MB (100.00%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.js
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.js
@@ -5,11 +5,9 @@
 var reductionOperations = function(nums) {
     let count = 0;
     nums.sort((a,b) =>b-a);
-  
     for (i = 0; i < nums.length - 1 ; i++ ) {
         const num = nums[i];
         const nextNum = nums[i + 1];
-
         if (num !== nextNum) {
             count += i + 1;
         }
