9dc98278ff3007cff6ca7bb60c5c67368699fe7f
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-11-21 13:47:42 +0200

    Time: 91 ms (75.14%), Space: 57.2 MB (61.38%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    int rev(int x){
+        int res = 0;
+        while(x > 0){
+            res = res*10 + x%10;
+            x /= 10;
+        }
+        return res;
+    }
+
+    int countNicePairs(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int ans = 0;
+
+        for(int num : nums){
+            int revNum = rev(num);
+            int diff = num - revNum;
+     
+            if(mp.find(diff) != mp.end()){
+                ans = (ans + mp[diff])%1000000007;
+
+            } 
+            mp[diff]++;
+        }
+        return ans;
+    }
+};
