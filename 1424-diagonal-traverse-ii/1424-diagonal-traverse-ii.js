b6b9b8675709a3664adf97f7d71ad935ec494e35
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-11-22 14:06:40 +0200

    Time: 220 ms (65.39%), Space: 110.7 MB (31.73%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.js
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.js
@@ -9,9 +9,8 @@ var findDiagonalOrder = function(nums) {
     for (let i = 0; i < rCount; i++) {
         for (let j = 0; j < nums[i].length; j++) {
             const check = map.get(i+j)
-            if (check) {
-                check.unshift(nums[i][j])
-            } else  map.set(i+j, [nums[i][j]])
+            if (check) check.unshift(nums[i][j])
+            else  map.set(i+j, [nums[i][j]])
             
         } 
     }
