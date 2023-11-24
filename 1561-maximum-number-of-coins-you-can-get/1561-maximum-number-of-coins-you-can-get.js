8de360cab6a746fa88d92a550e3f1d7868af3b9f
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-11-24 20:40:20 +0200

    Time: 184 ms (30.23%), Space: 52 MB (69.77%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.js
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.js
@@ -4,9 +4,8 @@
  */
 var maxCoins = function(piles) {
     piles.sort((a, b) => a - b);
-    let times = piles.length / 3;
-    let result = 0;
-    let index = piles.length - 2;
+    let times = piles.length / 3, result = 0 , index = piles.length - 2;
+   
     while(times > 0){
         result += piles[index];
         index-=2;
