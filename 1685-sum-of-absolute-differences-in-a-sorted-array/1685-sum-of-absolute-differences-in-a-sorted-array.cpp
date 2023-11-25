a692fc8aa7b0d09739509f78a1905f9a0d3e74b4
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-11-25 20:29:12 +0200

    Time: 78 ms (95.41%), Space: 87.7 MB (33.24%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -6,14 +6,12 @@ public:
         int i,c=0,s=0,n=nums.size();
 
         s = accumulate(nums.begin(),nums.end(),0);
-
         for(i=0;i<n;i++){
             int x = (nums[i]*(i)-c);
             int y = (s-c) - nums[i]*(n-i);
             ans.push_back(x+y);
             c+=nums[i];
         }
-
         return ans;
         
     }
