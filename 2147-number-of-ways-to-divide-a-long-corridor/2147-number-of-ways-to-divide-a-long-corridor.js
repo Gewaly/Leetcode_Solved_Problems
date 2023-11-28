3f3bea0cf915f5c665aea3422853f0f259349b2b
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-11-28 18:50:31 +0200

    Time: 101 ms (75.00%), Space: 66.6 MB (50.00%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} corridor
+ * @return {number}
+ */
+var numberOfWays = function(corridor) {
+    var pos=[]
+    for(let i=0;i<corridor.length;i++){
+        if(corridor[i]=='S'){
+            pos.push(i)
+        }
+    }
+    //console.log('pos=',pos)
+    if(pos.length%2!=0 || pos.length==0){
+        return 0
+    }
+    if(pos.length==2){
+        return 1
+    }
+    var combo=1
+    for(let i = 1;i<pos.length-2;i+=2){
+        //console.log('i=',i)
+        let diff = pos[i+1]-pos[i]
+        combo = (combo*diff)%1000000007
+        //console.log('diff=',diff,'combo=',combo)
+    }
+    return combo
+    
+
+
+};
