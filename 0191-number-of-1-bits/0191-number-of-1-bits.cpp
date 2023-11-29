68c9d1b841fad00e4f1a03c95949a93d62df1072
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-11-29 21:47:39 +0200

    Time: 0 ms (100.00%), Space: 6.5 MB (27.24%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-     int ones = 0;
-    	while(n!=0) {
-          //  cout<<ones<<" "<<n<<endl;
-    		ones = ones + (n & 1);
-    		n = n>>1;
-    	}
-    	return ones;
+           return __builtin_popcount(n);
+
     }
 };
