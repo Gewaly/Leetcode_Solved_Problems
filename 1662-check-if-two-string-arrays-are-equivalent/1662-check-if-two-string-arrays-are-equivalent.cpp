ff2c07b3f732caddd22d4aa8a35d0976c4366dad
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-01 20:41:35 +0200

    Time: 3 ms (71.01%), Space: 11.8 MB (77.65%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -5,7 +5,6 @@ public:
         for(int i=0;i<word1.size();i++){
             res1+=word1[i];
         }
-        cout<<res1<<endl;
          string res2="";
         for(int i=0;i<word2.size();i++){
             res2+=word2[i];
