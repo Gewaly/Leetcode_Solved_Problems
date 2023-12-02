b3e9c2b5ee5367e9836ea626e970d12e26aff347
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-02 11:36:51 +0200

    Time: 165 ms (21.90%), Space: 51.8 MB (23.13%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -3,15 +3,13 @@ public:
     int countCharacters(vector<string>& words, string chars) {
         int ans=0;
         unordered_map<char,int>ch;
-        for(int i=0;i<chars.length();i++){
-            ch[chars[i]]++;
-        }
+        for(int i=0;i<chars.length();i++)  ch[chars[i]]++;
+        
         for(int i=0;i<words.size();i++){
             unordered_map<char,int>str;
             string s=words[i];
-            for(int j=0;j<s.length();j++){
-                str[s[j]]++;
-            }
+            for(int j=0;j<s.length();j++) str[s[j]]++;
+            
             bool flag=true;
             for(int k=0;k<s.length();k++){
                 if(str[s[k]]>ch[s[k]]){flag=false;    break;  }
