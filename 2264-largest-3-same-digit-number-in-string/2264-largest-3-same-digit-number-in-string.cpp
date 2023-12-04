07f54cb9a125be6bc756c3925a501f47ce9928e4
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-04 21:00:39 +0200

    Time: 0 ms (100.00%), Space: 6.9 MB (31.59%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -5,9 +5,8 @@ public:
             string j="";
             int r,mx=-1;
       for(int i=0;i<num.length()-2;i++){
-        if(num[i]==num[i+1] && num[i]==num[i+2]){
-             mx=max(num[i]-'0',mx);
-                } 
+        if(num[i]==num[i+1] && num[i]==num[i+2]) mx=max(num[i]-'0',mx);
+                
     }
         if(mx!=-1){
         j=mx+'0',  j+=mx+'0',j+=mx+'0';
