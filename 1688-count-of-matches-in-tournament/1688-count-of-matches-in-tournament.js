bf17c4c3bcf083382e7af4b25529344e1f6ac01f
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-05 09:57:45 +0200

    Time: 44 ms (91.99%), Space: 41.7 MB (61.63%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    let count = 0;
+  count = 0;
     while(n>1){
         count = count + Math.floor(n/2);  n = Math.ceil(n/2);     
     }
