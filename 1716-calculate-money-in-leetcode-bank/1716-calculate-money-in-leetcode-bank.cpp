a007ed89c3565f6422c4c21264feb5ea1987a437
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-06 14:21:17 +0200

    Time: 3 ms (13.68%), Space: 6.4 MB (35.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -3,11 +3,7 @@ public:
     int totalMoney(int n) {
         int money = 0, count = 1, i = 0, counter = 1;
         while(n > 0){
-            if(i == 7){
-                counter++;
-                count = counter;
-                i = 0;
-            }
+            if(i == 7){ counter++; count = counter; i = 0;  }
             money += count;
             i++;
             count++;
