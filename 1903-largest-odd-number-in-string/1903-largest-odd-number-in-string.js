8f37550f6dfe3693a2e0d41ee03f335c7ec0905d
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-07 20:03:39 +0200

    Time: 79 ms (9.93%), Space: 53 MB (8.95%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -9,9 +9,9 @@ var largestOddNumber = function(num) {
      let k=num.length-1;
     for(let i=0;i<num.length;i++){
                 if(parseInt(num[k])%2!=0){
-                    for(i=0;i<=k;i++){
+                    for(i=0;i<=k;i++)
                          sub+=num[i];
-                    }
+                    
                     return sub;
                 } 
         else  k--;
