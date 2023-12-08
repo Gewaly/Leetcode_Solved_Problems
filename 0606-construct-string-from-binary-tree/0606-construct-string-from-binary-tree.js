6fcd8505cd9d88f29e723938bb664af4bdd8bdc1
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-08 21:29:17 +0200

    Time: 66 ms (44.74%), Space: 46.1 MB (95.39%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
@@ -12,9 +12,7 @@
  */
 var tree2str = function(root) {
     if (!root)   return '';
-    
     if (!root.left && !root.right)   return root.val + '';
-    
     if (!root.right)   return root.val + '(' + tree2str(root.left) + ')';
     
     return root.val + '(' + tree2str(root.left) + ')(' + tree2str(root.right) + ')';
