b4949ac2f2c9ad92342ec73671d2e4894ec95610
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-11 20:32:10 +0200

    Time: 45 ms (94.15%), Space: 43 MB (58.51%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var findSpecialInteger = function(arr) {
-    if (arr.length === 1) return arr[0];
     let count = 1;
+        if (arr.length === 1) return arr[0];
+
     for (let i = 1; i < arr.length; i++) {
         if (arr[i] === arr[i-1]) {
             count++;
