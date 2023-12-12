378e7545bc1813b980115f681880c1a9dfd94c00
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-12 10:04:06 +0200

    Time: 61 ms (26.88%), Space: 43 MB (42.29%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    nums.sort(function(a, b){return b-a});
-            return (nums[0]-1)*(nums[1]-1);
+    nums.sort(function(a,b){return b-a});
+     return (nums[0]-1)*(nums[1]-1);
 
 
     
