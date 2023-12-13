ddceef17b57e64bb4c810dcfcc3d7fc596d8cc51
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-13 21:11:47 +0200

    Time: 47 ms (100.00%), Space: 45.2 MB (57.14%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.ts
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.ts
@@ -10,10 +10,10 @@ function numSpecial(mat: number[][]): number {
 }
 
 function isSpecial(mat: number[][], vi: number, vj: number): boolean {
-    for (let j = 0; j < mat[0].length; j++) {
+    for (let j = 0; j < mat[0].length; j++) 
         if (j !== vj && mat[vi][j]) return false;
         
-    }
+    
     
     for (let i = 0; i < mat.length; i++) 
         if (i !== vi && mat[i][vj])  return false;
