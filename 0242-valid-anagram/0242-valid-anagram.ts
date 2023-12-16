16c3e2d3cc96f0c796c3735cce65d5037bacd3a0
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-16 13:34:13 +0200

    Time: 111 ms (11.10%), Space: 49 MB (30.60%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,7 +1,6 @@
 function isAnagram(s: string, t: string): boolean {
     t=t.split("").sort().join("");
         s=s.split("").sort().join("");
-    console.log(s," ",t);
         if(s==t) return true;
         return false;
     
