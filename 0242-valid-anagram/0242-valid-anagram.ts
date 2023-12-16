67727516517d97c3ba6868270200b80896a26b80
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-16 13:43:45 +0200

    Time: 85 ms (33.98%), Space: 49.3 MB (24.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,7 +1,6 @@
 function isAnagram(s: string, t: string): boolean {
     t=t.split("").sort().join("");
         s=s.split("").sort().join("");
-        if(s===t) return true;
-        return false;
+        return s==t;
     
 };
