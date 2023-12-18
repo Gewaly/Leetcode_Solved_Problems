cb0bbd77cd8af74263bcba9fde4808458dcf34a0
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-18 23:33:42 +0200

    Time: 27 ms (32.07%), Space: 20.8 MB (28.98%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-       long res=(nums[nums.size()-1]*nums[nums.size()-2])-(nums[0]*nums[1]);
-        return res;
+        return (nums[nums.size()-1]*nums[nums.size()-2])-(nums[0]*nums[1]);
         
     }
 };
