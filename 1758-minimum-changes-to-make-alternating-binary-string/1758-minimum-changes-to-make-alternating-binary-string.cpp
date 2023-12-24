e3f0b8bf327bee851dd297aa369286391f2ae8e2
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-24 22:02:51 +0200

    Time: 3 ms (78.14%), Space: 7.3 MB (86.53%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        
+        int zeroOnOdd  = 0,oneOnEven = 0,oneOnOdd=0,zeroOnEven = 0;
+        for( int i = 0 ; i < s.length(); i++)
+        {
+            if( i % 2 == 0)
+            {
+                if(s[i] == '0')oneOnEven += 1;
+                else zeroOnEven += 1;
+            }
+            else
+            {
+                if(s[i] == '0') oneOnOdd += 1;
+                else  zeroOnOdd += 1;
+            }
+        }
+
+        return min( zeroOnEven + oneOnOdd , oneOnEven + zeroOnOdd);
+    }
+};
