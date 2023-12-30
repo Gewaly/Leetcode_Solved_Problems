b18c409b4185fae33021a0c8d4a2c9dca4d4c0bf
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2023-12-30 13:35:55 +0200

    Time: 63 ms (5.06%), Space: 12.9 MB (10.12%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     bool makeEqual(vector<string>& words) {
         map<char,int>mp;
+        
         if(words.size()==1) return true;
+        
         for(int i=0;i<words.size();i++){
             string s =words[i];
             for(int j=0;j<s.size();j++){
