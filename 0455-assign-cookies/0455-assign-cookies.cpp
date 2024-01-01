21ffdff44e422a7ae8b5fcacb56b350c959a597e
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-01-01 19:42:53 +0200

    Time: 61 ms (6.00%), Space: 17.8 MB (36.11%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n=g.size(),m=s.size();
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int c=0;
+        int i=n-1,j=m-1;
+        while(i>=0 && j>=0){
+            if(g[i]<=s[j]){
+                c++;
+                i--;
+                j--;
+            }
+            else if(g[i]>s[j]){
+                i--;
+            }
+        }
+        return c;
+    }
+};
