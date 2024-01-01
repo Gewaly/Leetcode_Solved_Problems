9fa817bdb54c7fb4e32999d8320e2250131abdc5
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-01-01 19:43:23 +0200

    Time: 36 ms (10.24%), Space: 17.8 MB (75.56%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -7,11 +7,8 @@ public:
         int c=0;
         int i=n-1,j=m-1;
         while(i>=0 && j>=0){
-            if(g[i]<=s[j]){
-                c++;
-                i--;
-                j--;
-            }
+            if(g[i]<=s[j]) c++, i--,j--;
+            
             else if(g[i]>s[j]) i--;
             
         }
