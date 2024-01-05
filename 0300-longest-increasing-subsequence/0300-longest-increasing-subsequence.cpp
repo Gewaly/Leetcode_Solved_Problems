86feff5034da026e02751e141a339b2c828920b6
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-01-05 23:53:32 +0200

    Time: 9 ms (83.99%), Space: 10.6 MB (99.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,9 +5,8 @@ public:
         vector<int>sorted; 
         for(int i=0;i<n;i++){
             auto it = lower_bound(begin(sorted),end(sorted),nums[i]);
-            if(it == end(sorted)){
-                sorted.push_back(nums[i]);
-            }else *it = nums[i];
+            if(it == end(sorted))  sorted.push_back(nums[i]);
+        else *it = nums[i];
             
         }
         return sorted.size();
