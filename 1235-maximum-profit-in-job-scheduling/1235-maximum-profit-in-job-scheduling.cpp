50ba60d31fe77ac804304691a2263ed875144c43
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-01-06 15:48:05 +0200

    Time: 127 ms (82.23%), Space: 71.4 MB (71.52%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> dp;
     int solve(vector<pair<int, pair<int, int>>> &v, vector<int>& s, int i, int prev){
-        if(i>=v.size())return 0;
+        if(i>=v.size()) return 0;
         if(dp[i]!=-1)return dp[i];
         int t=0, nt=0;
         int ind=lower_bound(s.begin(), s.end(), v[i].second.first)-s.begin();
@@ -16,7 +16,6 @@ public:
         int n=s.size();
         dp.resize(n,  -1);
         for(int i=0; i<n; i++) v.push_back({s[i], {e[i], p[i]}});
-        
         sort(s.begin(), s.end());
         sort(v.begin(), v.end());
         return solve(v, s, 0, -1);
