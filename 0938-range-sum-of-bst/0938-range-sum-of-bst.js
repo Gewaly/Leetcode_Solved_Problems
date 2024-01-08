0f65857551ff7a7958f24d6fd0c1b0a0a341d778
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-01-08 23:36:37 +0200

    Time: 137 ms (29.92%), Space: 97 MB (30.19%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, L, R) {
+    var sum = 0;
+    if (root == null) {
+        return sum;
+    }
+
+    if (root.val > L) {
+        sum += rangeSumBST(root.left, L, R);
+    }
+    if (root.val <= R && root.val >= L) {
+        sum += root.val;
+    }
+    if (root.val < R) {
+        sum += rangeSumBST(root.right, L, R);    
+    }    
+    
+    return sum;
+};
