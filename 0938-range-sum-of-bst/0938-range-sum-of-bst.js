9cf8d09e9c98154d10e44d35acc9de242152fb4d
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-01-08 23:36:54 +0200

    Time: 116 ms (85.36%), Space: 97.2 MB (13.48%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.js
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.js
@@ -21,12 +21,11 @@ var rangeSumBST = function(root, L, R) {
     if (root.val > L) {
         sum += rangeSumBST(root.left, L, R);
     }
-    if (root.val <= R && root.val >= L) {
+    if (root.val <= R && root.val >= L) 
         sum += root.val;
-    }
-    if (root.val < R) {
-        sum += rangeSumBST(root.right, L, R);    
-    }    
+    
+    if (root.val < R)  sum += rangeSumBST(root.right, L, R);    
+    
     
     return sum;
 };
