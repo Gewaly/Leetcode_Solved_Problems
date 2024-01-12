559ac1b191a785bed5a3a38dbf7610374cdc3667
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-01-12 23:06:14 +0200

    Time: 5 ms (31.20%), Space: 6.9 MB (73.08%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -12,7 +12,6 @@ public:
                if(  s[i]=='a'|| s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' ||
               s[i]=='O' || s[i]=='U') cnt2++;
         
-        cout<<cnt1<<" "<<cnt2<<endl;
         return cnt1==cnt2;
         
     }
