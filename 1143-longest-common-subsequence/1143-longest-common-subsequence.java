08403ef089c6c958556511b6b3c291bb4f32d87b
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-01-25 21:47:39 +0200

    Time: 20 ms (82.35%), Space: 51 MB (27.26%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -6,10 +6,8 @@ class Solution {
         int lcs[][]=new int[m+1][n+1];
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1))
-                    lcs[i][j]=1+lcs[i-1][j-1];
-                else 
-                    lcs[i][j]=Math.max(lcs[i][j-1],lcs[i-1][j]);
+                if(text1.charAt(i-1)==text2.charAt(j-1)) lcs[i][j]=1+lcs[i-1][j-1];
+                else  lcs[i][j]=Math.max(lcs[i][j-1],lcs[i-1][j]);
             }
         }
         return lcs[m][n];
