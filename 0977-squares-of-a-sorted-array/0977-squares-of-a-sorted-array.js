59f7172d7378a6df67ce08ce55e3d28a17ef08f0
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-03-02 02:21:25 +0200

    Time: 79 ms (81.69%), Space: 55.9 MB (57.91%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -4,7 +4,7 @@
  */
 var sortedSquares = function(nums) {
     for(let i=0;i<nums.length;i++){
-        nums[i]=nums[i]**2;
+        nums[i]=nums[i]*nums[i];
     }
     nums.sort((a,b)=>a-b);
     return nums;
