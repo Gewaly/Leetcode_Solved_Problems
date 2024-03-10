3db103d924e72facfde576e43da9097d522d3ea7
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-03-10 13:19:19 +0200

    Time: 10 ms (20.45%), Space: 12.8 MB (41.99%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans,res;
+        vector<int>res;
         for(auto x: nums1){
             for(auto y: nums2){
                 if(x==y) res.push_back(x);
             }
         }
-        set<int>s;
-        for(auto x: res){
-            s.emplace(x);
-        }
-        for(auto y: s){
-            ans.push_back(y);
-        }
+        set<int>s(res.begin(),res.end());
+        vector<int>ans(s.begin(),s.end());
+        
         
         return ans;
         
