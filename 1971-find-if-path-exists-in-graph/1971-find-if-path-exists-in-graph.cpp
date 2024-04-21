46a40ef8dffb5470ecfe16362e9ece9e6cb52886
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-04-21 18:04:22 +0200

    Time: 743 ms (30.92%), Space: 229.1 MB (25.61%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -4,13 +4,10 @@ public:
         if(edges.size() == 0)  return true;
 
         
-        //  creating the adjustant list from edges
         unordered_map<int,list<int>> adjList;
                                                                                     
-        // no of edges
         int m = edges.size();
                                                                                     
-        // making adjustant list
         for(int i=0;i<m;i++){
             int u = edges[i][0];
             int v = edges[i][1];
@@ -19,13 +16,11 @@ public:
             adjList[v].push_back(u);
         }
         
-        // creating the visited map and queue for applying BFS algorithm
         unordered_map<int,bool> mp;
         queue<int> q;
         q.push(source);
         
         while(!q.empty()){
-            //  storing front element from queue and removing it from queue then
             int front = q.front();
             q.pop();
             
@@ -37,8 +32,7 @@ public:
             }
         }
         
-        // checking if visited array destination position is true or not
-        // if it is true means than it means their is possible path 
+       
         
         return mp[destination];
 
