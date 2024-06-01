47a423502b8cc5c8e852fed63c410176557a15f3
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-06-01 18:17:09 +0300

    Time: 5 ms (27.27%), Space: 7.9 MB (42.17%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum=0;
+        for(int i=0;i<s.size()-1;i++){
+        sum=sum+abs(s[i]-s[i+1]);
+        }
+        return sum;
+    }
+};
