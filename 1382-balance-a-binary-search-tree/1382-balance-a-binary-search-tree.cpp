da73fdd6bb6629602d469f251a92780c10b70d9b
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-06-26 21:44:11 +0300

    Time: 98 ms (42.42%), Space: 62.6 MB (60.81%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> vals;
+    TreeNode* balanceBST(TreeNode* root) {
+        trav(root);
+        return buildBST(0,vals.size()-1);
+    }
+    void trav(TreeNode* root){
+        if(root == nullptr)return;
+        trav(root->left);
+        vals.push_back(root->val);
+        trav(root->right);
+    }
+    TreeNode* buildBST(int l, int r){
+        if(l>r)return nullptr;
+        int mid = (l+r)/2;
+        TreeNode *root = new TreeNode(vals[mid]);
+        root->left = buildBST(l,mid-1);
+        root->right = buildBST(mid+1,r);
+        return root;
+    }
+};
