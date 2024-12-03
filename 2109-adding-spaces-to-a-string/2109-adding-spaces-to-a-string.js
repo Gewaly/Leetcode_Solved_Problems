403086fc9831d91bcfddb6fc90f738eef1923402
Mohamed Gewaly <103829071+Gewaly@users.noreply.github.com>
2024-12-03 10:01:11 +0200

    Time: 59 ms (89.09%), Space: 84.8 MB (85.48%) - LeetHub

---
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+var addSpaces = function (s, spaces) {
+    let temp = '', left = 0, right = 0, count = 0;
+    while (right < s.length) {
+        if (right === spaces[count]) {
+            temp += s.slice(left, right) + " ";
+            left = right;
+            count++;
+        }
+        right++;
+    }
+    temp += s.slice(left, right);
+    return temp;
+};
